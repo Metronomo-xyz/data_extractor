61e5f0d6f07a5fec2f836adfe842548743b9b09d
yaroslav <iaroslav@metronomo.xyz>
2022-10-02 00:19:02 +0500

    bugfix & restructure

---
+++ extract_mainnet_transactions.sh
@@ -0,0 +1,2 @@
+python3 -m extractor -a -t -n mainnet -s 01102022 -r 90
+python3 -m combiner -a -t -n mainnet -y 2022 -m "09 08 07"
--- scripts/extract_mainnet_transactions.sh
+++
@@ -1,1 +0,0 @@
-python -m data_extractor.extractor -t --network mainnet -l -s 23082022 -r 7
