c41575310753c591f67e123d602985f1158d6107
iarickbondarchuk <iaroslav@metronomo.xyz>
2022-11-29 19:47:07 +0000

    shutdown VM after execution

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
-nohup python3 -u -m extractor -t -a -s 29112022 -r 2 > extractor_log.txt 2> extractor_error.txt &
-nohup python3 -u -m aggregator -r 2 > agg_logs.txt 2> agg_error.txt &
-nohup python3 -u -m combiner > combiner_logs.txt 2> combiner_errors.txt &
+python3 -u -m extractor -t -a -s 29112022 -r
+python3 -u -m aggregator -r 2
+python3 -u -m combiner
+sudo shutdown -h now
