d803370893c22c1a545413776a8774d902a932e7
iarickbondarchuk <iaroslav@metronomo.xyz>
2022-11-30 11:07:02 +0200

    fix script

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
-start_time = `date +%s`
+start_time=`date +%s`
 
 python3 -u -m extractor -t -a -r 2
-extractor_time = `date +%s`
-echo extraction time was `expr $exrtactor_time - $start_time` s.
+extractor_time=`date +%s`
+echo extraction time was `expr $extractor_time - $start_time` s.
 
 python3 -u -m aggregator -r 2
-aggregator_time = `date +%s`
-echo aggregation time was `expr $aggregator_time - $exrtactor_time` s.
+aggregator_time=`date +%s`
+echo aggregation time was `expr $aggregator_time - $extractor_time` s.
 
 python3 -u -m combiner
-combiner_time = `date +%s`
+combiner_time=`date +%s`
 echo combining time was `expr $combiner_time - $aggregator_time` s.
 
-sudo shutdown -h now
+#sudo shutdown -h now
