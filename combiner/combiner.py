8fb448a8119e9bbb50336bca5a44871e5d4124c1
iarickbondarchuk <iaroslav@metronomo.xyz>
2022-12-05 21:35:12 +0200

    add module power-users and modules structure refactoring

--- combiner/combiner.py
+++ combiner/combiner.py
@@ -7,47 +7,25 @@ from itertools import combinations
 import sys
 from google.oauth2 import service_account
 
-def filter_blobs_by_year_month(blob_list, year, month):
-    return list(filter(lambda b: ((b.split("-")[1] == month) & (b.split("-")[0][-4:]  == str(year))), blob_list))
-
-def filter_blobs_by_entity(blob_list, network, entity, how):
-    return list(filter(lambda b: (c.BLOB_PATHS[network][how][entity] in b), blob_list))
-
-def filter_blobs_by_date(blob_list, date):
-    return list(filter(lambda b: b.split("_")[1].split("/")[2] == date, blob_list))
-
-def get_dataframe_from_blob(entity, bucket_name, blob_name, token_json_path):
-    return pd.read_csv("gs://" + bucket_name + "/" + blob_name,
-                       storage_options={"token": token_json_path})[c.ENTITIES[entity]["fields"]]
-
-def filter_blobs_by_dates(blob_list, dates_arr):
-    def dateToStr(d):
-        return d.strftime("%Y-%m-%d")
-
-    dates = list(map(dateToStr, dates_arr))
-    if ("action_receipt_actions" in blob_list[0]):
-        return list(filter(lambda b: b.split("_")[3].split("/")[1] in dates, blob_list))
-    if ("transactions" in blob_list[0]):
-        return list(filter(lambda b: b.split("_")[1].split("/")[2] in dates, blob_list))
 
 def combine_data(entities, network, dates, bucket, token_json_path, storage_client, how="daily"):
     bucket_name = bucket.name
     all_blobs = csu.get_blob_list(storage_client, bucket)
 
-    ara_blobs = filter_blobs_by_entity(all_blobs, network, "actions", how)
+    ara_blobs = csu.filter_blobs_by_entity(all_blobs, network, "actions", how)
 
 
-    ara_blobs = filter_blobs_by_dates(ara_blobs, dates)
+    ara_blobs = csu.filter_blobs_by_dates(ara_blobs, dates)
     print(ara_blobs)
 
-    tx_blobs = filter_blobs_by_entity(all_blobs, network, "transactions", how)
-    tx_blobs = filter_blobs_by_dates(tx_blobs, dates)
+    tx_blobs = csu.filter_blobs_by_entity(all_blobs, network, "transactions", how)
+    tx_blobs = csu.filter_blobs_by_dates(tx_blobs, dates)
     print(tx_blobs)
 
     tx_df = pd.DataFrame()
     for tx_b in tx_blobs:
         print(tx_b)
-        tx_data = get_dataframe_from_blob(
+        tx_data = csu.get_dataframe_from_blob(
             "transactions",
             bucket.name,
             tx_b,
@@ -60,7 +38,7 @@ def combine_data(entities, network, dates, bucket, token_json_path, storage_clie
     ara_df = pd.DataFrame()
     for ara_b in ara_blobs:
         print(ara_b)
-        ara_data = get_dataframe_from_blob(
+        ara_data = csu.get_dataframe_from_blob(
             "actions",
             bucket.name,
             ara_b,
@@ -79,8 +57,6 @@ def combine_data(entities, network, dates, bucket, token_json_path, storage_clie
     a = j.groupby(by="receiver_account_id").count().reset_index()
 
     print(a.head())
-
-
 
     print("joined")
     print("j size : " + str(sys.getsizeof(j) / 1024 / 1024 / 1024))
